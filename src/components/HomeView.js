baf591dca1f7c9286a5c19d5ea47582ac193eeb6
s217487971 <69649013+s217487971@users.noreply.github.com>
2021-12-01 13:57:56 +0200

    Updated HomePage
    Updated Home Page

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -17,8 +17,9 @@ function Homemethod()
     <p>SovTech creates Excellent Solutions using modern technologies that Ultimately have social Good just like other Tech Giants, however unlike other Tech Giants it does not do so at the expense of it's staff. After Reading The SovTech Spex it is Apparent that Sov-Tech Offers a Healthy Working Environment to its Staff and it would be blessing to Work at Sov-Tech. Regarding the Projects Created by Sov-Tech, all the Projects I have always wanted to be part and the Lessons are being done continously in the Real life Setting and It would Great Honor to Be Part of the Team and Contribute in the making of Great Solutions. Personally, growth and Improvement my Drive seeing how Sov-Tech is advancing into New Technologies Such as BlockChain it would be Great to learn about the Implementation of such Technologies in practice.</p>
     <p>Ever Since, modern Computers I have always wanted to be involved in the making of them, However things took a different turn in the age of smartphones. Ever since Smartphones Existed I wanted to Learn of they are made and possibly build my Softwares that could help People or Even improve exising Software. In my spare time I do try out new Frameworks and New Languages that are Being Used today Just to keep up as much as i can. I believe that Would be accelerated at Sov-Tech</p>
     <p>I Decided to Pursue a Degree in Computer Science so i could learn as much as I can and to try and learn everything there is to know about Software and Hardware, Even in University I realized that I could Understand Computing more than I understood Statistics, Mathematics and Applied Mathematics then I knew Computing is what I am best at. The Skills I have Aqcuired at School are decent start into the Software Engineering Field and there is Still more To Learn. At this Point I would love to work at Sov-Tech since I believe I will be an assert to SovTech and Equally SovTech will assist in the Growth of my Skills and Knowledge.</p>
-    <h5>Applications TO AUTOMATE TASKS</h5>
-    <h5>WEBSITES FOR YOUR BUSINESS</h5>
+    <h5>Let's Build Innovative Application</h5>
+    <h5>Let's Build Websites and Help Businesses</h5>
+    <h5>Let's Store Valuable Data</h5>
     <br/>
     <div class="card mb-3" >
   <div class="row g-0">
